c1aa5a6232d3b384f9d07aacb4eefe342a0bff7c
RohanRathod777 <rohangr7777@gmail.com>
2025-08-22 17:59:08 +0530

    control flow

---
+++ 04_Control_flow.js/One.js
@@ -0,0 +1,33 @@
+
+// COMPARISION OPERATORS : (< lessthan),(> Greaterthan),(<= lessthan equal to) (>= Greater than equal to) 
+// (== Equal to) (!= Not equal to) (=== Strict equality)
+
+//*********************************************************************************************************/
+
+if (2 === "2") {
+    //    console.log("Executed ");
+}
+// console.log("Not executed");
+
+
+if (2 != 3) {
+    //     console.log("Executed ");
+
+}
+
+const temprature = 40
+
+if (temprature < 50) {
+    // console.log("Temprature is less than 50");
+
+}
+else {
+    // console.log("Temprature is greater than 50");
+}
+
+const Score = 200
+if (Score > 100) {
+    let power = "fly"
+    console.log(`User power is : ${power}`);
+
+}
