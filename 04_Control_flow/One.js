6e55c7f406309abf6c2e7867b26e6c7642a0feec
RohanRathod777 <rohangr7777@gmail.com>
2025-08-23 10:28:08 +0530

    control flow

--- 04_Control_flow/One.js
+++ 04_Control_flow/One.js
@@ -32,14 +32,30 @@ if (Score > 100) {
 
 }
 
-const balance = 1000
-if (balance < 500) {
-    console.log("Less than 500");
+// const balance = 1000
+// if (balance < 500) {
+//     console.log("Less than 500");
 
-} else if (balance < 750) {
-    console.log("less than 750");
-} else if (balance < 900) {
-    console.log("less than 900");
-} else {
-    console.log("Greater than 1000");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("Greater than 1000");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const userLoggedInFromGoogle = false
+const userLoggedInFromGmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log("Allowed to buy course");
+
+}
+
+if (userLoggedInFromGoogle || userLoggedInFromGmail) {
+    console.log("User logged in");
+
 }
