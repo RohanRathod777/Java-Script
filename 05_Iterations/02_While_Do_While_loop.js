eab3883b8e1f21ca994837ee43a92458dca935fd
RohanRathod777 <rohangr7777@gmail.com>
2025-09-04 11:20:45 +0530

    While do while loop

---
+++ 05_Iterations/02_While_Do_While_loop.js
@@ -0,0 +1,24 @@
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index ${index}`);
+    index = index + 2
+
+}
+
+let myArray = ["Iron man", "Captain America", "Thor"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+
+
+}
+
+let Score = 1
+
+do {
+    console.log(`Score is ${Score}`);
+    Score++
+} while (Score <= 10);
