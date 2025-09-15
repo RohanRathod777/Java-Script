8129f9a9ee349ffe98cae324c52016988d9a4572
RohanRathod777 <rohangr7777@gmail.com>
2025-09-15 10:25:44 +0530

    digital clock js

--- 07_Projects/03_Digital_Clock/03_Digital_Clock.js
+++ 07_Projects/03_Digital_Clock/03_Digital_Clock.js
@@ -4,3 +4,7 @@ setInterval(function () {
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString();
 }, 1000);
+// Add this script to your HTML file, before </body>
+document.getElementById('clock').addEventListener('click', function() {
+    this.classList.toggle('glow-bg');
+});
