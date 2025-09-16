e807af0401980297afa4ff8ce351ddec5acb7b2f
RohanRathod777 <rohangr7777@gmail.com>
2025-09-16 13:23:04 +0530

    Guess The Number Project

---
+++ 07_Projects/04_Guess_The_Number/04_Guess_The_Number.js
@@ -0,0 +1,17 @@
+// console.log(parseInt(Math.random() * 100 + 1));
+
+const randomNumber = parseInt(Math.random() * 100 + 1)
+
+const Submit=document.querySelector('#subt')
+const userInput=document.querySelector('#guessField')
+const guesSlot=document.querySelector('#.guesses')
+const ramaining=document.querySelector('#.lastResult')
+const startOver=document.querySelector('#.Resultparas')
+
+let p = document.createElement('p')
+
+let prevGuess=[];
+let numGuess=1;
+
+let playGame=true;
+
